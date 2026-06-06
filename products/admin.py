9e8e97007a00240b2213c5db23e90c9d0ae01235
Parth1367 <namdevparth202@gmail.com>
2026-06-06 19:24:55 +0530

    BAS

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Category, Product, ProductVariant, ProductImage
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price")
+    prepopulated_fields = {"slug": ("name",)}
+    inlines = [ProductImageInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
 admin.site.register(ProductVariant)
-admin.site.register(ProductImage)
