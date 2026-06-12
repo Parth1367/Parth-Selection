186a57c00750e7264ad51a9489804ce58900f03a
Parth1367 <namdevparth202@gmail.com>
2026-06-12 21:25:42 +0530

    BAS 4

--- config/settings.py
+++ config/settings.py
@@ -119,9 +119,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
